cdca619b96158e8985f55ac853dca4ae186471ca
Vinicius Fortino <3rd.vinicius.fortino@amedigital.com>
2020-05-05 19:15:49 -0300

    using redux selector and resselector

--- src/components/CartTotal/index.js
+++ src/components/CartTotal/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-
+import { createSelector } from 'reselect';
 import * as CartActions from '../../store/actions/cart';
 
 const CartPrice = ({ cart, total, addProduct, setShipping }) => (
@@ -12,23 +12,43 @@ const CartPrice = ({ cart, total, addProduct, setShipping }) => (
             Itens: <strong>{cart.items.length}</strong>
         </p>
         <p>
-            Frete: <strong>{cart.shippingValue}</strong>
+            Shipping Value: <strong>{cart.shippingValue}</strong>
         </p>
         <p>
             Total:  <strong>{total}</strong>
         </p>
-        <button onClick={() => addProduct()}>Add Product</button>
-        <button onClick={() => setShipping()}>Calcular frete</button>
+        <button onClick={() => setShipping()}>Shipping Value calculator</button>
     </div>
 );
 
-const calculateTotal = (items) => {
+/*
+=== USING REDUX SELECTOR AND RESELECTOR ======
+
+Without the createSelector, every state update, the calculateTotal function will load,
+but when we use createSelector, it will only load when the state.items changes.
+
+ const calculateTotal = (items) => {
     return items.reduce((subtotal, item) => subtotal + item.price, 0)
-}
+ }
+
+So, uwe need to show to createSelector in the first function arg
+    state => state.items,
+and the second function arg is the function to calculate
+    items => items.reduce((subtotal, item) => subtotal + item.price, 0) 
+
+P.S.: We must pass all state to the function that will use createSelector
+
+*/
+const calculateTotal = createSelector(
+    state => state.items,
+    state => state.shippingValue,
+    (items, shippingValue) => items.reduce((subtotal, item) => subtotal + item.price + shippingValue, 0)
+);
+
 
 const mapStateToProps = ({ cart }) => ({
     cart,
-    total: calculateTotal(cart.items)
+    total: calculateTotal(cart)
 })
 
 const mapDispacthToProps = dispatch =>
