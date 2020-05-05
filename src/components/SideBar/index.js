c818a22d8f85bc601c8e3ec80f78ae691237daed
Vinicius Fortino <3rd.vinicius.fortino@amedigital.com>
2020-05-05 15:29:52 -0300

    reducers and actions less verbose refac

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import * as CourseActions from '../../store/actions/course';
 
-const SideBar = ({ modules, dispatch }) => (
+const SideBar = ({ modules, toggleLesson }) => (
     <aside>
         <h1>Modules</h1>
         {
@@ -13,7 +15,7 @@ const SideBar = ({ modules, dispatch }) => (
                     <ul>
                         {module.lessons.map(lesson => (
                             <li key={lesson.id}>{lesson.title}
-                                <button onClick={() => dispatch(CourseActions.toggleLesson(module, lesson))}>
+                                <button onClick={() => toggleLesson(module, lesson)}>
                                     Select
                                 </button>
                             </li>
@@ -25,8 +27,28 @@ const SideBar = ({ modules, dispatch }) => (
     </aside>
 );
 
+const mapStateToProps = state => ({
+    modules: state.course.modules
+})
+
+/*
+We use mapDispacthToProps to map the dispacth functions into a propertie on the scope
+
+We use bindActionCreator when the arguments on the dispach function as the same 
+as the action function will receive. Insted we use the function above:
+
+    toggleLesson: (module, lesson) =>
+        dispatch(CourseActions.toggleLesson(module, lesson))
+
+We use that another one:
+
+    bindActionCreators(CourseActions, dispatch);
+
+as our toggleLesson in the scope has the same value on the CourseActions, that will works
+*/
+const mapDispacthToProps = dispatch =>
+    bindActionCreators(CourseActions, dispatch);
+
 // connect uses the concpet high order component, pattern to share data between components
 // connect receive as first argument an function, a function that returns state properties
-export default connect(state => ({
-    modules: state.course.modules
-}))(SideBar)
+export default connect(mapStateToProps, mapDispacthToProps)(SideBar)
